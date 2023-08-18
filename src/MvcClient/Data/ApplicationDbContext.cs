744654cc0bad88acc47c171d5dddbefc1b96b4db
Pasha Pashazade | Novum LLC <pasha.pashazade@novum.tech>
2023-08-18 13:56:20 +0400

    Fix IdentityServer

--- src/MvcClient/Data/ApplicationDbContext.cs
+++ src/MvcClient/Data/ApplicationDbContext.cs
@@ -1,11 +1,16 @@
 
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MvcClient.Identity;
 
 namespace MvcClient.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        
+         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
     }
 }
